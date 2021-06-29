3a8eb816e500edd53c4e90dd1fdc0c5538e9bf43
Muhammad Al Farizzi <malfarizzi13@gmail.com>
2021-06-29 16:18:27 +0700

    Filter Tahap Dua

--- routes/api.php
+++ routes/api.php
@@ -6,4 +6,6 @@ use Illuminate\Support\Facades\Route;
 Route::get("check-referal", "Frontend\CostumerController@CheckReferal");
 Route::get('/get-vendor-dalam-kota', "Customer\CheckoutController@checkoutTahap1");
 Route::get('/cek-ongkir', "Customer\CheckoutController@checkOngkir");
-Route::get('/filter-kategori', 'HomeController@filterKategori');
+Route::get('/filter-kategori', 'FilterController@filterKategori');
+Route::get('/filter-sorting', 'FilterController@filterSorting');
+Route::get('/filter-harga', 'FilterController@filterHarga');
